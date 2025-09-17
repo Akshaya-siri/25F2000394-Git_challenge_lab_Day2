2cede7d73e4e5fe0504f56c5dbe0004167a39bd8
Akshaya-siri <akshayasiri.m@gmail.com>
2025-09-17 22:36:32 +0530

    Add main.py module for multiplication

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ def add(a, b):
 def sub(a, b):
     return a-b
 
+def mul(a, b):
+    return a*b
+
 if __name__ == "__main__":
     x = int(input("Enter first number: "))
     y = int(input("Enter second number: "))
     print("Sum =", add(x, y))
     print("Sub =", sub(x, y))
+    print("Mul =", mul(x, y))
